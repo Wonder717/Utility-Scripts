d704179c7942f1b5b38e5144812d28127849799a
Wonder <wonderdermwin@yahoo.com>
2023-01-14 07:20:48 +0000

    sqs

---
+++ Cloud Providers/AWS/Python Scripts/sqs_create.py
@@ -0,0 +1,8 @@
+import boto3
+
+
+def create_queue(queue_name):
+	sqs = boto3.resource('sqs')
+	queue = sqs.create_queue(QueueName=‘queue_name')
+        #The new queue's url identifier
+	print(queue.url)
